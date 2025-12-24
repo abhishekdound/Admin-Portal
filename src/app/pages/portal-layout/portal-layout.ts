c08c61a7ae34c17f414d3cef727932417e8d8691
Abhishek <abhishekdound@gmail.com>
2025-12-24 16:05:25 +0530

    Admin Portal  Project Created

---
+++ src/app/pages/portal-layout/portal-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-layout',
+  imports: [],
+  templateUrl: './portal-layout.html',
+  styleUrl: './portal-layout.scss',
+})
+export class PortalLayout {
+
+}
