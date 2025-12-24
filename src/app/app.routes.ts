20156f8b5119f7886085efe641b9897087f71d40
Abhishek <abhishekdound@gmail.com>
2025-12-24 18:14:36 +0530

    Basic Design added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'app',pathMatch:'full'},
+  {path:'app',loadComponent:()=>import('./pages/portal-layout/portal-layout').then((c)=>c.PortalLayout),
+    children:[
+      {path:'',redirectTo:'dashboard',pathMatch:'full'},
+      {path:'dashboard',loadComponent:()=>import('./pages/main-content/dashboard/dashboard').then((c)=>c.Dashboard)}
+    ]
+  }
+];
--- src/app/pages/portal-layout/portal-layout.ts
+++ src/app/pages/portal-layout/portal-layout.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from "./header/header";
+import { SideBar } from "./side-bar/side-bar";
+import { RouterOutlet } from '@angular/router';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-portal-layout',
-  imports: [],
+  imports: [Header, SideBar, RouterOutlet, Footer],
   templateUrl: './portal-layout.html',
   styleUrl: './portal-layout.scss',
 })
