1bb0f388d311babe10f452a8662833ae35b37ea8
Abhishek <abhishekdound@gmail.com>
2025-12-27 20:09:37 +0530

    datatable added with dummy data

---
+++ src/app/interface/user-data.interface.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  name: string;
+  age: number;
+  gender: 'Male' | 'Female';
+  mail: string;
+  image: string;
+  dob: string; 
+}
--- src/app/pages/main-content/users/users.ts
+++ src/app/pages/main-content/users/users.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DashboardHeader } from "../../portal-layout/dashboard-header/dashboard-header/dashboard-header";
+import { User } from '../../../interface/user-data.interface';
+import { UsersData } from '../../../constants/user-data.constants';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { SortType } from '@swimlane/ngx-datatable';
+
 
 @Component({
   selector: 'app-users',
-  imports: [DashboardHeader],
+  imports: [DashboardHeader,NgxDatatableModule],
   templateUrl: './users.html',
   styleUrl: './users.scss',
 })
 export class Users {
+  userData=signal<User[]>(UsersData);
+  
+  sortType = SortType.multi;
+
+  columns = [
+  { prop: 'id' },
+  { prop: 'name' },
+  { prop: 'age' },
+  { prop: 'gender' },
+  { prop: 'mail' }
+];
 
 }
