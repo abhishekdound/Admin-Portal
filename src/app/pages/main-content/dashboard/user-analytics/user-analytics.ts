db25c1818ca4a89c43b8cce9f17ec1216d6df10f
Abhishek <abhishekdound@gmail.com>
2026-01-03 19:42:34 +0530

    user-analytics based on age added with proper design

--- src/app/pages/main-content/dashboard/user-analytics/user-analytics.ts
+++ src/app/pages/main-content/dashboard/user-analytics/user-analytics.ts
@@ -11,17 +11,25 @@ import { UserStorageService } from '../../users/user-storage-service/user-storag
   styleUrl: './user-analytics.scss',
 })
 export class UserAnalytics implements OnInit {
+  chartView: [number, number] = [600, 300];
   users: User[] = [];
   genderChartData: any[] = [];
+  
+  ageChartData: any[] = [];
 
   constructor(private userStorage: UserStorageService) {}
 
   ngOnInit() {
+    
+  this.updateChartSize();
+  window.addEventListener('resize', this.updateChartSize.bind(this));
     this.users = this.userStorage.getUsers();
 
     console.log('Users loaded:', this.users); // 👈 ADD THIS
 
     this.prepareGenderChart();
+    
+    this.prepareAgeChart();
   }
 
   prepareGenderChart() {
@@ -35,4 +43,27 @@ export class UserAnalytics implements OnInit {
 
     console.log('Chart data:', this.genderChartData); // 👈 ADD
   }
+
+  private prepareAgeChart() {
+    const age = (u: User) => u.age;
+
+    this.ageChartData = [
+      { name: '18–22', value: this.users.filter(u => age(u) >= 18 && age(u) <= 22).length },
+      { name: '23–27', value: this.users.filter(u => age(u) >= 23 && age(u) <= 27).length },
+      { name: '28–35', value: this.users.filter(u => age(u) >= 28 && age(u) <= 35).length },
+      { name: '36+', value: this.users.filter(u => age(u) >= 36).length }
+    ];
+  }
+  updateChartSize() {
+  const width = window.innerWidth;
+
+  // adjust based on layout
+  if (width > 1400) {
+    this.chartView = [600, 300];
+  } else if (width > 992) {
+    this.chartView = [500, 300];
+  } else {
+    this.chartView = [350, 300];
+  }
+}
 }
